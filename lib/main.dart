8c818cfb35e66faa4ca331c9f8943ec47a6d66a3
basit-h <basit.hussain47@gmail.com>
2023-02-17 10:00:24 +0500

    Initial

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Flutter 30 days chellange',
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Day 1'),
+      ),
+      body: const Center(
+        child: Text('Flutter 30 days challenge'),
+      ),
+    ),
+  ));
+}
